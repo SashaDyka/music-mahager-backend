5fce7a92f7cff044d5492cd70e221eddca574f9f
SashaDyka <beli.rothi2005@gmail.com>
2025-08-27 22:51:18 +0300

    edit auth middleware

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,26 +1,20 @@
 import type { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
-const secret = process.env.JWT_SECRET as string;
-
-interface JwtPayload {
-    userId: number;
-}
+const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret';
 
 export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
-    const token = req.cookies.jwt;
+    const token = req.cookies.token; 
 
     if (!token) {
-        return res.status(401).json({ message: 'Authorization token not found.' });
+        return res.status(401).json({ message: 'Unauthorized: No token provided' });
     }
 
     try {
-        const decoded = jwt.verify(token, secret) as JwtPayload;
-        // @ts-ignore
-        req.user = decoded; 
+        const decoded = jwt.verify(token, JWT_SECRET);
+        // req.user = decoded; // Attach user data to the request object
         next();
     } catch (error) {
-        res.clearCookie('jwt'); 
-        res.status(401).json({ message: 'Invalid or expired token.' });
+        res.status(401).json({ message: 'Unauthorized: Invalid token' });
     }
 };
