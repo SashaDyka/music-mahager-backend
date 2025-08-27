e30e01b9a1941ccc0eeb513b016a440996e27bd8
SashaDyka <beli.rothi2005@gmail.com>
2025-08-27 22:50:48 +0300

    edit auth routes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+ import { Router } from 'express';
 import * as authController from '../controllers/authController.js';
 import { requireAuth } from '../middleware/authMiddleware.js';
 
@@ -10,5 +10,9 @@ router.post('/auth/login ', authController.login);
 router.post('/auth/logout', authController.logout);
 router.get('/auth/me', authController.getProfile);
 
+router.get('/auth/me', requireAuth, (req, res) => {
+    // In a real project req.user 
+    res.json({ message: 'This is a protected route', user: 'user_info_here' });
+})
 
 export { router as authRoutes };
