b55b9c0205e76128bea8b5d73791e7acdb504977
SashaDyka <beli.rothi2005@gmail.com>
2025-09-03 23:20:32 +0300

    feat: add interface

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -3,7 +3,15 @@ import jwt from 'jsonwebtoken';
 
 const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret';
 
-export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
+interface JwtPayload {
+    userId: string;
+}
+
+interface AuthenticatedRequest extends Request {
+    user?: JwtPayload;
+}
+
+export const requireAuth = (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
     const token = req.cookies.token; 
 
     if (!token) {
@@ -11,10 +19,11 @@ export const requireAuth = (req: Request, res: Response, next: NextFunction) => 
     }
 
     try {
-        const decoded = jwt.verify(token, JWT_SECRET);
-        // req.user = decoded; // Attach user data to the request object
+        const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+        req.user = decoded;
+        
         next();
     } catch (error) {
-        res.status(401).json({ message: 'Unauthorized: Invalid token' });
+        return res.status(401).json({ message: 'Unauthorized: Invalid token' });
     }
 };
