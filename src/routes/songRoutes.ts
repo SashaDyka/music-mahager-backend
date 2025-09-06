a430ccf525c905afda815d10fb7c5c643b105bdb
SashaDyka <beli.rothi2005@gmail.com>
2025-09-06 23:20:30 +0300

    feat: update user routes

--- src/routes/songRoutes.ts
+++ src/routes/songRoutes.ts
@@ -1,15 +1,14 @@
 import { Router } from 'express';
-import * as songsController from '../controllers/songController.js';
+import { songController } from '../controllers/songController.js';
 import { uploadAudio } from '../middleware/uploadMiddleware.js';
 import { requireAuth } from '../middleware/authMiddleware.js';
-import type { authRoutes } from './authRoutes.js';
 
 const router = Router();
 
 router.get('/', requireAuth, songController.getSongs); 
-router.post('/', requireAuth, upload.single('audioFile'), songsController.createSong);
-router.get('/:id', requireAuth, songsController.getSongDetails);
-router.patch('/:id', requireAuth, songsController.updateSong);
-router.delete('/:id', requireAuth, songsController.deleteSong);
+router.post('/', requireAuth, uploadAudio('audioFile'), songsController.createSong);
+router.get('/:id', requireAuth, songController.getSongById);
+router.patch('/:id', requireAuth, songController.updateSong);
+router.delete('/:id', requireAuth, songController.deleteSong);
 
 export { router as songRoutes };
