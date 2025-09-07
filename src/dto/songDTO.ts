da53499c5b1c199aa0d1667645173d387dd6bd53
SashaDyka <beli.rothi2005@gmail.com>
2025-09-07 14:24:42 +0300

    feat: edit response song dto

--- src/dto/songDTO.ts
+++ src/dto/songDTO.ts
@@ -1,13 +1,17 @@
-export class SongResponseDto {
-  id: number;
-  title: string;
-  artist: string;
-  playlistId?: number;
+import type { Song } from "@prisma/client";
 
+export class SongResponseDto {
+  id: string;
+  title: string;
+  durationSec: number;
+  sourceType: string;
+  audioUrl: string;
+  
   constructor(song: any) {
     this.id = song.id;
     this.title = song.title;
-    this.artist = song.artist;
-    this.playlistId = song.playlistId;
+    this.durationSec = song.durationSec;
+    this.sourceType = song.sourceType;
+    this.audioUrl = song.audioUrl;
   }
 }
