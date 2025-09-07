6b22c71c664c20fc0e1f4eaf56072b02b1d9d0b8
SashaDyka <beli.rothi2005@gmail.com>
2025-09-07 19:21:15 +0300

    edit user routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
-import * as userController from '../controllers/userController.js';
+import { UserController } from '../controllers/userController.js';
+import { UserService } from '../services/userService.js';
 import { requireAuth } from '../middleware/authMiddleware.js';
-
+import { prisma } from '../prismaClient.js';
 
 const router = Router();
 
-router.get('/users');
-router.post('/users');
+const userService = new UserService(prisma);
+const userController = new UserController(userService);
 
+router.get('/:id', (req, res) => userController.getUserById(req, res));
+router.patch('/:id', requireAuth, (req, res) => userController.updateUser(req, res));
+router.delete('/:id', requireAuth, (req, res) => userController.deleteUser(req, res));
 
 export { router as userRoutes };
