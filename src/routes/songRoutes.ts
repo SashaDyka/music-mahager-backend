835dfda019ea4b0c62535b7665197298b6d61005
SashaDyka <beli.rothi2005@gmail.com>
2025-09-07 17:45:16 +0300

    edit song routes

--- src/routes/songRoutes.ts
+++ src/routes/songRoutes.ts
@@ -1,14 +1,19 @@
 import { Router } from 'express';
-import { songController } from '../controllers/songController.js';
-import { uploadAudio } from '../middleware/uploadMiddleware.js';
+import { SongController } from '../controllers/songController.js';
+import { SongService } from '../services/songService.js';
 import { requireAuth } from '../middleware/authMiddleware.js';
+import { uploadAudio } from '../middleware/uploadMiddleware.js';
+import { prisma } from '../prismaClient.js';
 
 const router = Router();
 
-router.get('/', requireAuth, songController.getSongs); 
-router.post('/', requireAuth, uploadAudio('audioFile'), songsController.createSong);
-router.get('/:id', requireAuth, songController.getSongById);
-router.patch('/:id', requireAuth, songController.updateSong);
-router.delete('/:id', requireAuth, songController.deleteSong);
+const songService = new SongService(prisma);
+const songController = new SongController(songService);
+
+router.get('/', requireAuth, (req, res) => songController.getSongs(req, res));
+router.post('/', requireAuth, uploadAudio.single('audioFile'), (req, res) => songController.createSong(req, res));
+router.get('/:id', requireAuth, (req, res) => songController.getSongById(req, res));
+router.patch('/:id', requireAuth, (req, res) => songController.updateSong(req, res));
+router.delete('/:id', requireAuth, (req, res) => songController.deleteSong(req, res));
 
 export { router as songRoutes };
