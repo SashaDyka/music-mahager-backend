3e9d3eecdf16f753c89f3d151db26e1ed0b407ad
SashaDyka <beli.rothi2005@gmail.com>
2025-09-07 19:00:59 +0300

    edit media routes

--- src/routes/mediaRoutes.ts
+++ src/routes/mediaRoutes.ts
@@ -1,12 +1,22 @@
 import { Router } from 'express';
-import * as mediaController from '../controllers/mediaController.js';
-import { requireAuth } from '../middleware/authMiddleware.js';
 import multer from 'multer';
+import { MediaController } from '../controllers/mediaController.js';
+import { requireAuth } from '../middleware/authMiddleware.js';
+import { MediaService } from '../services/mediaService.js';
+import { prisma } from '../prismaClient.js';
 
-const upload = multer({ dest: 'uploads/' }); 
 const router = Router();
+const mediaService = new MediaService(prisma);
+const mediaController = new MediaController(mediaService);
 
-router.post('/upload', requireAuth, upload.single('audioFile'), mediaController.uploadFile);
-router.get('/file/:id', mediaController.getFile);
+const upload = multer({ dest: 'uploads/' });
+
+router.post('/upload', requireAuth, upload.single('audioFile'), 
+  (req, res) => mediaController.uploadFile(req, res)
+);
+
+router.get('/file/:id',
+  (req, res) => mediaController.getFile(req, res)
+);
 
 export { router as mediaRoutes };
